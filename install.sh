83671de0a117bcf5adda92b549e0e64da4efc55a
MauricioF-cpnv <33344875+MauricioF-cpnv@users.noreply.github.com>
2019-05-24 08:49:33 +0200

    Update and rename install to install.sh

--- install.sh
+++ install.sh
@@ -49,3 +49,14 @@ certbot certonly --webroot --agree-tos --email $mail --no-eff-email -d $domain
 
 apt-get install fail2ban
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+
+cd /etc/fail2ban
+wget https://raw.githubusercontent.com/MauricioF-cpnv/nginx-config-odoo/master/jail.local
+
+cd /etc/fail2ban/filter.d/
+wget https://raw.githubusercontent.com/MauricioF-cpnv/nginx-config-odoo/master/nginx-odoo.local
+wget https://raw.githubusercontent.com/MauricioF-cpnv/nginx-config-odoo/master/nginx-conn-limit.conf
+wget https://raw.githubusercontent.com/MauricioF-cpnv/nginx-config-odoo/master/nginx-req-limit.conf
+
+service fail2ban restart
+fail2ban-client status
